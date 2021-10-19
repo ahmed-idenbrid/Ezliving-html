cc5fd8bdea3c60947b1523a9a761ca839cfde8e8
ahmed-idenbrid <84811071+ahmed-idenbrid@users.noreply.github.com>
2021-10-19 09:43:08 +0500

    intial commit

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+var swiper = new Swiper(".EzFeaturedSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1366: {
+      slidesPerView:4,
+      spaceBetween: 20,
+    },
+  },
+});
+var swiper = new Swiper(".EzPartnerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1366: {
+      slidesPerView:3,
+      spaceBetween: 20,
+    },
+  },
+});
+var swiper = new Swiper(".EzNewComerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1366: {
+      slidesPerView:4,
+      spaceBetween: 20,
+    },
+  },
+});
